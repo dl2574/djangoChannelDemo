488a42709ca12ed634dab79ddff8607843edc282
dl2574 <20144415+dl2574@users.noreply.github.com>
2024-05-14 21:48:39 -0700

    Create chat app

--- channelDemo/asgi.py
+++ channelDemo/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter # Added for channels to work
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelDemo.settings")
 
-application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = get_asgi_application(
+    {
+        "http": django_asgi_app,
+    }
+)
--- channelDemo/settings.py
+++ channelDemo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "chat.apps.ChatConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -67,6 +68,7 @@ TEMPLATES = [
     },
 ]
 
+# ASGI_APPLICATION = "channelDemo.asgi.application"
 WSGI_APPLICATION = "channelDemo.wsgi.application"
 
 
